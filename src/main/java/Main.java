84fc2c2ca2c465ad9b6a58152c80dec6e678144f
nils grimonprez <grimonpreznils@gmail.com>
2023-01-09 17:25:19 +0100

    change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import redis.clients.jedis.Jedis;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,6 +14,15 @@ public class Main {
         // Récupération de la valeur stockée dans Redis
         String value = jedis.get("mykey");
         System.out.println(value);
+
+        // Création et initialisation d'une nouvelle clé-valeur dans Redis
+        jedis.lpush("maListe", "valeur1");
+        jedis.lpush("maListe", "valeur2");
+        jedis.lpush("maListe", "valeur3");
+
+        // Récupération de la liste dans Redis
+        List<String> list = jedis.lrange("maListe", 0, -1);
+        System.out.println(list); // affiche ["valeur3", "valeur2", "valeur1"]
 
         // Fermeture de la connexion à la base de données
         jedis.close();
